b040b85b479556b1c09f2b4f5678bfa1a3b3f10c
AnnVerba <verbaanna01@gmail.com>
2022-10-24 17:24:28 +0300

    Commit

--- src/movie/movie.module.ts
+++ src/movie/movie.module.ts
@@ -3,15 +3,24 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { Movie } from './models/movie.model';
 import { MovieService } from './movie.service';
 import { MovieController } from './movie.controller';
-//import { ClientsModule, Transport } from '@nestjs/microservices';
+
 import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
 import { MessagingService } from './MessagingService';
-//import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
   imports: [
     SequelizeModule.forFeature([Movie]),
 
+    RabbitMQModule.forRoot(RabbitMQModule, {
+      exchanges: [
+        {
+          name: 'exchange1',
+          type: 'topic',
+        },
+      ],
+
+      uri: 'amqp://localhost:5672',
+    }),
     MovieModule,
   ],
 
