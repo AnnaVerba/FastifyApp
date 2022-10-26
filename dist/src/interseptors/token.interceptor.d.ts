c078799ff9b202da8e43037faf0f158f36ba81bb
AnnaVerba <115066712+AnnaVerba@users.noreply.github.com>
2022-10-26 11:13:15 +0300

    Delete dist directory

--- dist/src/interseptors/token.interceptor.d.ts
+++
@@ -1,9 +0,0 @@
-import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
-import { Observable } from 'rxjs';
-import { AuthService } from '../registration/auth/auth.service';
-import { UserEntity } from '../registration/model/user.entity';
-export declare class TokenInterceptor implements NestInterceptor {
-    private readonly authService;
-    constructor(authService: AuthService);
-    intercept(context: ExecutionContext, next: CallHandler<UserEntity>): Observable<UserEntity>;
-}
--- dist/src/upload/uploadFile/upload.controller.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="multer" />
-export declare class UploadController {
-    uploadedFile(file: Express.Multer.File): Promise<void>;
-}
